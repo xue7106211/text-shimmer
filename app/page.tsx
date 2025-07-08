7cc20152b6b363cfcb147868babeb14785909adc
薛困惑 <xue7106211@gmail.com>
2025-07-08 11:25:40 +0800

    修复 Vercel 部署

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { TextShimmer } from "../components/ui/text-shimmer";
 
 export default function Home() {
@@ -6,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <TextShimmer className="text-xl">
-          How heavy the days are, There's not a fire that can warm me...
+          How heavy the days are, There&apos;s not a fire that can warm me...
         </TextShimmer>
       </main>
     </div>
